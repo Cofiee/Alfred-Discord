eb3d6d18bda870eb3f5d24c3a0ddc6b18c1905f3
Cofiee <39025622+Cofiee@users.noreply.github.com>
2021-04-19 21:58:40 +0200

    Asynchroniczne usuwanie wzmianek

--- src/main.js
+++ src/main.js
@@ -7,13 +7,26 @@ const channels = client.channels.cache;
 client.on("ready", () => {
     console.log("I am ready!");
 });
-
+/*
 client.on("message", (message) => {
     if(message.channel.name === "testybota" &&
         message.author.bot === false)
     {
         message.reply("I'm here to serve");
         myServerLog("Alfred replied on " + message.author.username + " message at " + message.channel.name)
+    }
+});
+*/
+client.on("message", async (message) => {
+    if(message.channel.name === "testybota" &&
+        message.author.bot === false &&
+        message.type === "DEFAULT" &&
+        message.mentions.members.size > 0)
+    {
+        await setTimeout(() => {
+            message.delete();
+          }, 5000);
+        myServerLog("Alfred deleted " + message.author.username + " mention at \"" + message.channel.name + "\"");
     }
 });
 
