370947a4bbcf14ed511e32bbcdf7e74e92f05a72
CofieeCake <39025622+Cofiee@users.noreply.github.com>
2021-05-27 19:21:57 +0200

    Dodano tworzenie kanalu na reakcje

--- src/ticket_system/ticketManager.js
+++ src/ticket_system/ticketManager.js
@@ -1,3 +1,5 @@
+const { User } = require("discord.js");
+
 /**
  * Autor: Cofiee
  * Wersja: 1.0
@@ -18,51 +20,64 @@ module.exports = function(discord, settings, client, channels) {
     this.ticketList = [];
     this.ticketCollectorList = [];
 
+    /**
+     * 
+     */
     this.prepareTicketSystem = async () => {
         const targetChannel = this.channels.find(channel => channel.name === this.settings.TicketChannelName);
-        if(targetChannel.type === "text")
-        {
-            targetChannel.messages.fetch().then(messages => {
-                targetChannel.bulkDelete(messages);
-            }).catch(err => {
-                console.log('Error while doing Bulk Delete');
+        if(targetChannel.type !== "text") return;
+        targetChannel.messages.fetch().then(messages => {
+           targetChannel.bulkDelete(messages);
+        }).catch(err => {
+            console.log('Error while doing Bulk Delete');
+            console.log(err);
+        });
+        let createTicketEmbeded = new this.Discord.MessageEmbed();
+        createTicketEmbeded.setTitle(this.ticketCreator.title);
+        createTicketEmbeded.setDescription(this.ticketCreator.description);
+        createTicketEmbeded.setColor(this.ticketCreator.color);
+        targetChannel.send(createTicketEmbeded).then(message => {
+            this.ticketCreationMessage = message;
+            message.react(this.ticketCreator.reaction)
+            .catch(err => {
+                console.log('Error reacting');
                 console.log(err);
             });
-            let createTicketEmbeded = new this.Discord.MessageEmbed();
-            createTicketEmbeded.setTitle(this.ticketCreator.title);
-            createTicketEmbeded.setDescription(this.ticketCreator.description);
-            createTicketEmbeded.setColor(this.ticketCreator.color);
-            targetChannel.send(createTicketEmbeded).then(message => {
-                this.ticketCreationMessage = message;
-                message.react(this.ticketCreator.reaction)
-                .catch(err => {
-                    console.log('Error reacting');
-                    console.log(err);
-                });
-            });
-        }
+        });
+        /*
+        const filter = (reaction, user) => {
+            return reaction.emoji.name === "📩";
+        };
+        this.ticketCreationMessage.awaitReactions(filter)
+        */
     }
+
 
     /**
      * Listener reaguje na reakcje na wiadomosci this.ticketCreationMessage
      * Tworzy kanal tekstowy sluzaczy do obslugi zgloszenia
      * TODO: PRZEROBIC NA COLLECTOR REAKCJI
      */
-    client.on('reaction', (reaction) => {
-        if(this.ticketCreationMessage === null) return;
-        if(reaction.message !== this.ticketCreationMessage) return;
-        if(reaction !== this.ticketCreator.reaction)
+    client.on('messageReactionAdd', (reaction, user) => {
+        if(reaction.me === true) 
+            return;
+        if(this.ticketCreationMessage === null) 
+            return;
+        if(reaction.message !== this.ticketCreationMessage) 
+            return;
+        if(reaction.emoji.name !== this.ticketCreator.reaction)
         {
-            reaction.message.reactions.cache.get(reaction).remove()
-            .catch(error => console.error('Failed to clear reactions'));
+            reaction.remove().catch(error => console.error('Failed to clear reactions'));
+            return;
         }
-        const everyoneRole = client.guilds.get('SERVER ID').roles.find('name', '@everyone');
-        const author = reaction.author;
-        reaction.message.guild.createChannel(author.username, 'text')
+        const guild = reaction.message.guild;
+        const everyoneRole = guild.roles.everyone;
+        guild.channels.create(user.username, 'text')
             .then(c => {
-                c.overwritePermissions(author.id, {VIEW_CHANNEL: true});
+                c.setParent(this.ticketCreator.ticket_system_cat_id);
+                c.overwritePermissions(user.id, {VIEW_CHANNEL: true});
                 c.overwritePermissions(client.id, {VIEW_CHANNEL: true});
-                //TODO: ROLA MANAGERA
+                c.overwritePermissions(this.ticketSettings.responsible_role_id, {VIEW_CHANNEL: true}); //Czlonek zarzadu
                 c.overwritePermissions(everyoneRole, {VIEW_CHANNEL: false});
                 let createTicketEmbeded = new this.Discord.MessageEmbed();
                 createTicketEmbeded.setTitle("PLACEHOLDER TITLE");
@@ -71,17 +86,24 @@ module.exports = function(discord, settings, client, channels) {
                     this.ticketList.push(message);
                     message.react(this.ticketSettings.closing_reaction)
                     .catch(err => {
-                        console.log("Error while reacting ticket")
+                        console.log("Error while reacting ticket");
                         console.log(err);
                     });
-                    
-                    this.ticketCollectorList.push(collector);
+                    //const collector = this.ticketReactionsCollectorCreator(message);
+                    //this.ticketCollectorList.push(collector);
 
                 });
             })
-            .catch(error => console.error('Failed to create ticket channel'));  
+            .catch(err => {
+                console.error('Failed to create ticket channel');
+                console.error(err);
+            });
+        reaction.remove().catch(err => console.error('Failed to clear reactions'));
     });
 
+
+
+    /*
     this.ticketReactionsCollectorCreator = (message) => {
         const collectorFilter = (reaction, user) => reaction.emoji.name === this.ticketSettings.closing_reaction;
         let collector = message.createReactionCollector(collectorFilter, {time: 15000});
@@ -94,7 +116,7 @@ module.exports = function(discord, settings, client, channels) {
             }
             //TODO: USUWANIE KANALU !!!!! KRYTYCZNE
         });
+        return collector;
     }
-
-
+    */
 }
